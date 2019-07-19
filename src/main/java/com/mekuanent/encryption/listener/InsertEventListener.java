6a336113d9bba24e9ba083a74d4cece87366e8e8
Mekuanent Kassaye <mekuanent@hotmail.com>
2019-07-19 05:06:38 -0500

    - convert to one place encryption event listener

--- src/main/java/com/mekuanent/encryption/listener/InsertEventListener.java
+++ src/main/java/com/mekuanent/encryption/listener/InsertEventListener.java
@@ -2,11 +2,10 @@ package com.mekuanent.encryption.listener;
 
 import com.mekuanent.encryption.Weaver;
 import com.mekuanent.encryption.annotation.Encrypted;
-import com.mekuanent.encryption.handler.EncryptionHandler;
 import com.mekuanent.encryption.handler.IEncryptionHandler;
 import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.event.spi.*;
-import org.hibernate.persister.entity.EntityPersister;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -14,6 +13,7 @@ import org.springframework.stereotype.Component;
 import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 
 /**
@@ -22,25 +22,11 @@ import java.util.Map;
  * @author Mekuanent Kassaye
  */
 @Component
-public class InsertEventListener implements PreInsertEventListener, PreUpdateEventListener,
-        FlushEntityEventListener, PreCollectionUpdateEventListener {
+public class InsertEventListener implements
+        FlushEntityEventListener {
 
-    private static final Logger logger = LoggerFactory.getLogger(LoadEventListener.class);
-    public static final LoadEventListener INSTANCE =
-            new LoadEventListener();
+    private static final Logger logger = LoggerFactory.getLogger(InsertEventListener.class);
 
-    /**
-     * triggered Just before an entity is inserted, invoking the com.mekuanent.encryption algorithm on annotated fields
-     * of the entity
-     *
-     * @param preInsertEvent an event object containing the entity
-     */
-    @Override
-    public boolean onPreInsert(PreInsertEvent preInsertEvent) {
-
-        return process(preInsertEvent.getEntity());
-
-    }
 
     /**
      * handles annotation processing and com.mekuanent.encryption
@@ -49,46 +35,47 @@ public class InsertEventListener implements PreInsertEventListener, PreUpdateEve
     private boolean process(final Object entity){
 
         List<Field> fields = Weaver.getAnnotatedFields(entity, Encrypted.class);
+
+        AtomicBoolean result = new AtomicBoolean(true);
+
         fields.forEach(field -> {
             String text = Weaver.getContent(entity, field);
             if(text != null && !text.isEmpty()){
+
+                if(text.contains("\\@{encrypted}@//")) result.set(false);
 
                 IEncryptionHandler handler = Weaver.decideEncryptionHandler(field);
 
                 if(handler != null){
                     String cipher = handler.encrypt(text);
                     if(cipher != null){
-                        Weaver.setContent(entity, field, cipher);
+                        Weaver.setContent(entity, field, "\\@{encrypted}@//"+cipher);
                     }
                 }
             }
         });
 
-        return false;
+        return result.get();
     }
+
+
+    private static boolean flushed = false;
 
     /**
-     * triggered Just before an entity is updated, invoking the com.mekuanent.encryption algorithm on annotated fields
+     * triggered Just before an entity is inserted, invoking the com.mekuanent.encryption algorithm on annotated fields
      * of the entity
      *
-     * @param preUpdateEvent an event object containing the entity
+     * @param flushEntityEvent an event object containing the entity
      */
     @Override
-    public boolean onPreUpdate(PreUpdateEvent preUpdateEvent) {
-        return process(preUpdateEvent.getEntity());
-    }
-
-    @Override
     public void onFlushEntity(FlushEntityEvent flushEntityEvent) throws HibernateException {
-        process(flushEntityEvent.getEntity());
-        flushEntityEvent.setDirtyProperties(new int[]{0, 1, 2});
-        int[] dirty = flushEntityEvent.getDirtyProperties();
-        flushEntityEvent.setDirtyCheckPossible(true);
-        flushEntityEvent.setHasDirtyCollection(true);
-    }
 
-    @Override
-    public void onPreUpdateCollection(PreCollectionUpdateEvent preCollectionUpdateEvent) {
-        System.out.println("sandsad");
+
+        Object entity = flushEntityEvent.getEntity();
+        Session session = flushEntityEvent.getSession();
+
+        if(process(entity)) {
+            session.flush();
+        }
     }
 }
